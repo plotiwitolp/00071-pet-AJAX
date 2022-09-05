dc5e543f33f3d51731b25b6a8be7b0158f2487f6
plotiwitolp <plotiwitolp@gmail.com>
2022-09-06 00:16:04 +0400

    fix

--- index.php
+++ index.php
@@ -5,15 +5,19 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Document</title>
+  <title>00071-pet-AJAX</title>
   <link rel="stylesheet" href="./style.css" />
 </head>
 
 <body>
-
-  <input type="button" id="show_ip" value="Показать мой IP">
-  <div id="myip"></div>
-
+  <div>
+    <input type="button" id="show_ip" value="Показать мой IP с помощью vanilla JS">
+    <div id="myip"></div>
+  </div>
+  <div>
+    <input type="button" id="show_ip2" value="Показать мой IP с помощью jQuery">
+    <div id="myip2"></div>
+  </div>
   <script src="./jquery-3.6.0.min.js"></script>
   <script src="./script.js"></script>
 </body>
--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+/**
+ * AJAX on vanilla JS
+ */
+
 window.onload = function () {
-  document.querySelector('#show_ip').addEventListener('click', function () {
-    ajaxGet();
-  });
+  document.querySelector('#show_ip').addEventListener('click', getAjaxGetVanillaJS);
+  document.querySelector('#show_ip2').addEventListener('click', getAjaxGetJQuery);
 };
+
+function getAjaxGetVanillaJS() {
+  ajaxGet();
+}
+function getAjaxGetJQuery() {
+  const request = new XMLHttpRequest();
+  request.onreadystatechange = function () {
+    if (request.readyState == 4 && request.status == 200) {
+      document.querySelector('#myip2').innerHTML = request.responseText;
+    }
+  };
+  request.open('GET', './ajaxtest.php');
+  request.send();
+}
 
 function ajaxGet() {
   const request = new XMLHttpRequest();
@@ -14,3 +31,13 @@ function ajaxGet() {
   request.open('GET', './server.php');
   request.send();
 }
+
+/**
+ * AJAX on jQuery
+ */
+$.ajax({
+  url: 'ajaxtest.php',
+  success: function () {
+    getAjaxGetJQuery;
+  },
+});
